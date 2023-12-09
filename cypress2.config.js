2ec42d9c109db41b90799f5f2ecfb02ea8ace57a
Serg130618 <ssa0618@yandex.ru>
2023-12-09 11:06:14 +0300

    1

---
+++ cypress2.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    // retries: 2,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    viewportWidth: 846,
+    viewportHeight: 414,
+  },
+};
